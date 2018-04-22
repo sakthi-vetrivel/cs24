677bc8ed07d86cfbfcb59243850f81d367af3060
sakthi-vetrivel <svetrive@caltech.edu>
2018-04-22 16:13:12 -0700

    creating allocator file

---
+++ cs24hw3-svetrive/myalloc/myalloc.c
@@ -0,0 +1,114 @@
+/*! \file
+ * Implementation of a simple memory allocator.  The allocator manages a small
+ * pool of memory, provides memory chunks on request, and reintegrates freed
+ * memory back into the pool.
+ *
+ * Adapted from Andre DeHon's CS24 2004, 2006 material.
+ * Copyright (C) California Institute of Technology, 2004-2010.
+ * All rights reserved.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "myalloc.h"
+
+
+/*!
+ * These variables are used to specify the size and address of the memory pool
+ * that the simple allocator works against.  The memory pool is allocated within
+ * init_myalloc(), and then myalloc() and free() work against this pool of
+ * memory that mem points to.
+ */
+int MEMORY_SIZE;
+unsigned char *mem;
+
+
+/* TODO:  The unacceptable allocator uses an external "free-pointer" to track
+ *        where free memory starts.  If your allocator doesn't use this
+ *        variable, get rid of it.
+ *
+ *        You can declare data types, constants, and statically declared
+ *        variables for managing your memory pool in this section too.
+ */
+static unsigned char *freeptr;
+
+
+/*!
+ * This function initializes both the allocator state, and the memory pool.  It
+ * must be called before myalloc() or myfree() will work at all.
+ *
+ * Note that we allocate the entire memory pool using malloc().  This is so we
+ * can create different memory-pool sizes for testing.  Obviously, in a real
+ * allocator, this memory pool would either be a fixed memory region, or the
+ * allocator would request a memory region from the operating system (see the
+ * C standard function sbrk(), for example).
+ */
+void init_myalloc() {
+
+    /*
+     * Allocate the entire memory pool, from which our simple allocator will
+     * serve allocation requests.
+     */
+    mem = (unsigned char *) malloc(MEMORY_SIZE);
+    if (mem == 0) {
+        fprintf(stderr,
+                "init_myalloc: could not get %d bytes from the system\n",
+		MEMORY_SIZE);
+        abort();
+    }
+
+    /* TODO:  You can initialize the initial state of your memory pool here. */
+    freeptr = mem;
+}
+
+
+/*!
+ * Attempt to allocate a chunk of memory of "size" bytes.  Return 0 if
+ * allocation fails.
+ */
+unsigned char *myalloc(int size) {
+
+    /* TODO:  The unacceptable allocator simply checks to see if there are at
+     *        least "size" bytes left in the pool, and if so, the caller gets
+     *        the current "free-pointer" value, and then freeptr is incremented
+     *        by size bytes.
+     *
+     *        Your allocator will be more sophisticated!
+     */
+    if (freeptr + size < mem + MEMORY_SIZE) {
+        unsigned char *resultptr = freeptr;
+        freeptr += size;
+        return resultptr;
+    }
+    else {
+        fprintf(stderr, "myalloc: cannot service request of size %d with"
+                " %lx bytes allocated\n", size, (freeptr - mem));
+        return (unsigned char *) 0;
+    }
+}
+
+
+/*!
+ * Free a previously allocated pointer.  oldptr should be an address returned by
+ * myalloc().
+ */
+void myfree(unsigned char *oldptr) {
+    /* TODO:
+     *
+     * The unacceptable allocator does nothing -- that's part of why this is
+     * unacceptable!
+     *
+     * Allocations will succeed for a little while...
+     */
+}
+
+/*!
+ * Clean up the allocator state.
+ * All this really has to do is free the user memory pool. This function mostly
+ * ensures that the test program doesn't leak memory, so it's easy to check
+ * if the allocator does.
+ */
+void close_myalloc() {
+    free(mem);
+}
